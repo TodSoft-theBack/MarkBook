cd5b4fccecd1e992722f8da4335e7c9c33409161
Teodor Nikolov <tediesuper@gmail.com>
2021-03-23 20:53:34 +0200

    Добавих валидация към оценките (markDAO)

--- Services/DAO/MarkDAO.cs
+++ Services/DAO/MarkDAO.cs
@@ -44,29 +44,99 @@ namespace Services.DAO
 
         public int RemoveMark(int id)
         {
+            if (id == 0)
+            {
+                throw new ArgumentException("This mark does not exist");
+            }
+
+            var mark = this.context.Marks.Where(m => m.MarkId == id).FirstOrDefault();
+
+            if (mark == null)
+            {
+                throw new ArgumentException("This mark does not exist");
+            }
+
             context.Remove(context.Marks.Where(m => m.MarkId == id).FirstOrDefault());
             return context.SaveChanges();
         }
         public Marks GetMarkById(int markId)
         {
-            return this.context.Marks
-                       .FirstOrDefault(m => m.MarkId == markId);
+            if (markId == 0)
+            {
+                throw new ArgumentException("This mark does not exist");
+            }
+
+            var mark = this.context.Marks.Where(m => m.MarkId == markId).FirstOrDefault();
+            if (mark == null)
+            {
+                throw new ArgumentException("This mark does not exist");
+            }
+
+            return this.context.Marks.FirstOrDefault(m => m.MarkId == markId);
         }
         public List<Marks> GetAllMarksOfStudentById(int studentId)
         {
-            var marks = this.context.Marks
-                .Where(st => st.StudentId.Equals(studentId))
+            if (studentId == 0)
+            {
+                throw new ArgumentException("This student does not exist");
+            }
+
+            var student = this.context.Students.Where(st => st.StudentId == studentId);
+
+            if (student == null)
+            {
+                throw new ArgumentException("This student does not exist");
+            }
+
+            List<Marks> marks = this.context.Marks
+                .Where(st => st.StudentId == studentId)
                 .ToList();
+
+            if (marks == null)
+            {
+                throw new ArgumentException("This student does not have any marks");
+            }
+
             return marks;
         }
         public List<Marks> GetMarksForGivenSubjectById(int subjectId, int studentId)
         {
+            if (subjectId == 0)
+            {
+                throw new ArgumentException("This subject does not exist");
+            }
+
+            var subject = this.context.Subjects.Where(s => s.SubjectId == subjectId).FirstOrDefault();
+            if (subject == null)
+            {
+                throw new ArgumentException("This subject does not exist");
+            }
+
             return this.context.Marks
               .Where(s => s.SubjectId == subjectId && s.StudentId == studentId)
               .ToList();
         }
         public List<Marks> GetMarksForGivenSubjectById(int subjectId)
         {
+            if (subjectId == 0)
+            {
+                throw new ArgumentException("This subject does not exist");
+            }
+
+            Subjects subject = this.context.Subjects.Where(s => s.SubjectId == subjectId).FirstOrDefault();
+
+            if (subject == null)
+            {
+                throw new ArgumentException("This subject does not exist");
+            }
+
+            List<Marks> marks = this.context.Marks.Where(m => m.SubjectId == subjectId).ToList();
+
+            if (marks.Count == 0)
+            {
+                throw new ArgumentException("There are no marks related to the selected subject");
+            }
+
             return this.context.Marks
                 .Include(s => s.Student)
                 .Where(s => s.SubjectId.Equals(subjectId))
